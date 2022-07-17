66d1001d6388ebf61bb152ccce34088c97cadd7a
Zach Sanford <sanfordzachary@gmail.com>
2022-07-17 16:57:27 -0700

    Added the Save Settings Method to the ConfigurationService service. Unit tests are passing.

--- NewsScout.Source/Services/ConfigurationService.cs
+++ NewsScout.Source/Services/ConfigurationService.cs
@@ -26,6 +26,24 @@ namespace NewsScout.Services
             }            
         }
 
+        // Save to settings.json from a Settings class object
+        public static void SaveSettings(Settings? _settingsToSave)
+        {
+            string _fileName = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Configuration\settings.json");
+
+            try
+            {
+                string _jsonContent = JsonSerializer.Serialize<Settings>(_settingsToSave, new JsonSerializerOptions() { WriteIndented = true });
+                File.WriteAllText(_fileName, _jsonContent);
+            }
+            catch (Exception e)
+            {
+                WriteLine($"Here is the error message:\n\n{e.Message}");
+                throw;
+            }
+        }
+
         #endregion
+
     }
 }
