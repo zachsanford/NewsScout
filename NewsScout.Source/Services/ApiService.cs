78b3d5dcd374f43078ed47e635288bf3d5761275
Zach Sanford <sanfordzachary@gmail.com>
2022-07-27 10:45:33 -0700

    Added ApiResponse model. Built ApiService and created the GetNews() method. Tested; Working.

---
+++ NewsScout.Source/Services/ApiService.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Json;
+
+namespace NewsScout.Services
+{
+    public class ApiService
+    {
+
+        #region Methods
+
+        // General Request
+        public static async Task<ApiResponse> GetNews()
+        {
+            Settings _settings = ConfigurationService.LoadSettings();
+            ApiResponse? _returnResponse = new ApiResponse();
+
+            using (HttpClient _client = new HttpClient())
+            {
+                HttpRequestMessage _request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://newsdata.io/api/1/news?apikey={_settings.ApiKey}")
+                };
+
+                using (HttpResponseMessage _response = await _client.SendAsync(_request))
+                {
+                    _response.EnsureSuccessStatusCode();
+                    return _returnResponse = await _response.Content.ReadFromJsonAsync<ApiResponse>();
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ NewsScout.Tests/ApiServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using NewsScout.Models;
+using NewsScout.Services;
+
+namespace NewsScout.Tests
+{
+    [TestClass]
+    public class ApiServiceTests
+    {
+        [TestMethod]
+        public async Task TestGetNews_Correct()
+        {
+            // Arrange & Act
+            ApiResponse testResponse = await ApiService.GetNews();
+
+            // Assert
+            Assert.IsNotNull(testResponse);
+        }
+    }
+}
